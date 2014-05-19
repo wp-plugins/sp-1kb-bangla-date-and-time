f38b422130e51ddd5fd085c7eb7e65bb3af6d7eb
hmbashar <hmbashar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-19 08:21:23 +0000

    Uploading version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/sp-1kb-bangla-date-and-time/trunk@916991 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: SP 1KB Bangla Date And Time
+Plugin URI: http://www.shaplahost.com/plugins/sp-bangla-date-and-time-converter
+Description: This is just any wordpress site English language all date and time convert to bangla language.
+Version: 1.0
+Author: Md Abul Bashar
+Author URI: http://www.pchelpcenterbd.com/
+*/
+
+
+function sp_bangla_date_and_time_convert($str)
+{
+    $engNumber = array(1,2,3,4,5,6,7,8,9,0);
+    $bangNumber = array('১','২','৩','৪','৫','৬','৭','৮','৯','০');
+    $converted = str_replace($engNumber, $bangNumber, $str);
+  
+    return $converted;
+}
+  
+add_filter( 'get_the_time', 'sp_bangla_date_and_time_convert' );
+add_filter( 'the_date', 'sp_bangla_date_and_time_convert' );
+add_filter( 'get_the_date', 'sp_bangla_date_and_time_convert' );
+add_filter( 'comments_number', 'sp_bangla_date_and_time_convert' );
+add_filter( 'get_comment_date', 'sp_bangla_date_and_time_convert' );
+add_filter( 'get_comment_time', 'sp_bangla_date_and_time_convert' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== SP 1KB Bangla Date And Time ===
+Tags: English to bangla, Bangla Date, Bangla Time, Bangla Date and time.
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is just any wordpress site English language all date and time convert to bangla language.
+
+== Description ==
+
+This is just any wordpress site English language all date and time convert to bangla language.
+
+
+== Installation ==
+
+* Just Upload `SP 1KB Bangla Date And Time` to the `/wp-content/plugins/` directory
+* Activate the plugin through the 'Plugins' menu in WordPress
+* And enjoy this plugin.
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release.
